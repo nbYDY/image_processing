8bfdf6c91a4314a1fd4e44cc6146c833cf735e0a
杨东谕 <743807504@qq.com>
2019-11-15 21:38:44 +0800

    first commit

---
+++ first/generateFigure.m
@@ -0,0 +1,36 @@
+function [img] = generateFigure(imgW, imgH)
+img = zeros(imgH,imgW,3);
+for i = 1:3
+    img(:,:,i)= 255;
+end
+img(:,1,:)=0;
+
+%记录0-2pi的各个坐标信息
+x = 0: 2*pi/imgW: 2*pi; 
+red = sin(x);
+green = cos(x);
+blue = x.^2;
+
+% 把y映射到图片的高
+red = (1-red)/2*(imgH / (4 * pi * pi) - 1)+1;
+red = imgH - red;
+red = round(red);
+green = (1-green)/2*(imgH / (4 * pi * pi) - 1)+1;
+green = imgH - green;
+green = round(green);
+maxb = max(blue);
+minb = min(blue);
+blue = (maxb - blue) / (maxb - minb) * (imgH -1) + 1;
+blue = round(blue);
+x = 1 : imgW;
+for i = 1: imgW
+    img(red(i),x(i),2) = 0;
+    img(red(i),x(i),3) = 0;
+    img(green(i),x(i),1) = 0;
+    img(green(i),x(i),3) = 0;
+    img(blue(i),x(i),1) = 0;
+    img(blue(i),x(i),2) = 0;
+end
+
+imshow(img);
+end
